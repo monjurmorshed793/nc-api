eb82e9883934a6386714fb52e256fa3de87fa0c4
monjurmorshed793 <monjurmorshed793@gmail.com>
2022-05-01 23:45:14 +0600

    initial nc-api project set up

---
+++ src/main/java/com/shorna/nutritionistapi/NutritionistApiApplication.java
@@ -0,0 +1,13 @@
+package com.shorna.nutritionistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shorna/nutritionistapi/NutritionistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shorna.nutritionistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
